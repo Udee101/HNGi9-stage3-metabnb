061f71b432ca83f14083bd060b8e62d2683eba73
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-14 06:40:45 +0100

    feat: array of nfts for the homepage

---
+++ src/Data/HomeNFT.js
@@ -0,0 +1,85 @@
+import nft1 from '../assets/nft1.png';
+import nft2 from '../assets/nft2.png';
+import nft3 from '../assets/nft3.png';
+import nft4 from '../assets/nft4.png';
+import nft5 from '../assets/nft5.png';
+import nft6 from '../assets/nft6.png';
+import nft7 from '../assets/nft7.png';
+import nft8 from '../assets/nft8.png';
+import Rating from '../assets/rating.svg'
+
+const Nfts = [
+  {
+    image: nft1,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  },
+
+  {
+    image: nft2,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  },
+
+  {
+    image: nft3,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  },
+
+  {
+    image: nft4,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  },
+
+  {
+    image: nft5,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  },
+
+  {
+    image: nft6,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  },
+
+  {
+    image: nft7,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  },
+
+  {
+    image: nft8,
+    name: "Desert King",
+    distance: "2345km away",
+    price: "1MBT per night",
+    availability: "available for 2weeks stay",
+    rating: Rating,
+  }
+]
+
+export default Nfts;
