f8aa108724e1cff8604d6640dcfff608f280707f
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-14 06:43:33 +0100

    feat: create section for more nfts

---
+++ src/components/MoreNfts.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import GroupImg from '../assets/groupImg.png';
+
+
+const MoreNfts = () => {
+  return (
+    <div className='more_nft my-10 w-full mx-auto md:flex md:flex-col md:justify-center'>
+      <div>
+        <img src={GroupImg} alt="" className='' />
+      </div>     
+    </div>
+  )
+}
+
+export default MoreNfts
