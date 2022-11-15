c4e4d57f035a09d4736f42eae9554bf509fec595
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-15 23:25:59 +0100

    feat: implement location filter for nfts

--- src/pages/PlaceToStay.jsx
+++ src/pages/PlaceToStay.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import FilterLocation from '../components/FilterLocation'
+import Location from '../components/Location'
 import PlaceToStayNFTs from '../components/PlaceToStayNFTs'
 
 const PlaceToStay = () => {
   return (
-    <div className='app mx-auto mb-20 mt-56'>
-      <PlaceToStayNFTs />
-    </div>
+    <>
+      <div className='app mx-auto mb-20 mt-44'>
+        <div className=' w-11/12 sm:flex sm:justify-between sm:items-center mb-10'>
+        <Location/>
+        <FilterLocation />
+        </div>
+        <PlaceToStayNFTs />
+      </div>
+    </>
   )
 }
 
