a0e1be5987277071bb374ba3d9ec9c27b1037410
Udeorah Frank <franklynudeorah@yahoo.com>
2022-11-16 00:21:15 +0100

    style: remove dark theme from location filter

--- src/components/FilterLocation.jsx
+++ src/components/FilterLocation.jsx
@@ -12,7 +12,7 @@ const FilterLocation = () => {
     <div className="relative inline-block">
     {/* Dropdown toggle button */}
 
-      <div onClick={toggleDropdown} className=" z-10 p-2 text-gray-700 bg-white border border-gray-300 rounded-lg dark:text-white focus:border-blue-500 focus:ring-opacity-40 focus:ring-blue-300  focus:ring  focus:outline-none">
+      <div onClick={toggleDropdown} className=" z-10 p-2 text-gray-700 bg-white border border-gray-300 rounded-lg focus:border-blue-500 focus:ring-opacity-40 focus:ring-blue-300  focus:ring  focus:outline-none">
         <p className=' cursor-pointer flex text-base'>Location
           <img src={FilterButton} alt="filter-button" className="ml-8" />
         </p>  
@@ -22,7 +22,7 @@ const FilterLocation = () => {
       {
       isOpen ? 
       (
-        <div className=" absolute lg:right-0 -right-36 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl dark:bg-gray-800"
+        <div className=" absolute lg:right-0 -right-36 z-20 w-48 py-2 mt-2 bg-white rounded-md shadow-xl"
         >
           <>
             <a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> Resturant</a><a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> Cottage </a><a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> Castle </a><a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> fantast city </a><a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> beach </a><a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> Carbins </a><a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> off-grid </a><a href="#" className="block px-4 py-3 text-sm text-gray-600 capitalize transition-colors duration-300 transform  hover:bg-gray-100 "> Farm </a>
